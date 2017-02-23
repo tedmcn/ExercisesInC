0b1736ae395c2571340416dd1f880eed66ed2b4c
tedmcn <tedmcnu@gmail.com>
2017-02-23 02:35:07 +0000

    2/22/17

---
+++ exercises/ex03/tee.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+    char search_for[80];
+
+    /* take input from the user and search */
+    while(1==1){
+        printf(">");
+        fgets(search_for, 80, stdin);
+    }
+    
+    return 0;
+}
